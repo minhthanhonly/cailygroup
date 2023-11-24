c09ab482b4f8ef81f6b24f252f4096322f02b2fc
Caily.Thanh <thanhonly@caily.com.vn>
2023-11-24 10:56:12 +0700

    Update

--- src/renderer/components/Table/Table_01/DatabaseTable_Rows.tsx
+++ src/renderer/components/Table/Table_01/DatabaseTable_Rows.tsx
@@ -1,27 +1,36 @@
-import React, { useEffect , useState } from 'react';
-import { startOfMonth, endOfMonth, eachDayOfInterval, format ,isToday , isSameDay } from 'date-fns';
-import { ButtonPrimary, ButtonSecondary ,ButtonThird, ButtonEdited} from '../../Button/Button';
-
-
+import React, { useEffect, useState } from 'react';
+import {
+  startOfMonth,
+  endOfMonth,
+  eachDayOfInterval,
+  format,
+  isToday,
+  isSameDay,
+} from 'date-fns';
 
 let DatabaseTable_Rows = () => {
-
-
   let firstDayOfMonth = startOfMonth(new Date()); // ngày đầu tháng
-  let lastDayOfMonth = endOfMonth(new Date());   // ngày cuối cùng của tháng
+  let lastDayOfMonth = endOfMonth(new Date()); // ngày cuối cùng của tháng
 
-  let daysOfMonth = eachDayOfInterval({ start: firstDayOfMonth, end: lastDayOfMonth });
+  let daysOfMonth = eachDayOfInterval({
+    start: firstDayOfMonth,
+    end: lastDayOfMonth,
+  });
 
   // Thêm một số ngày vào đầu tiên của danh sách để nó hiển thị ở cột đầu tiên
-  let paddingDays = Array.from({ length: firstDayOfMonth.getDay() }, (_, index) =>
-    new Date(firstDayOfMonth.getFullYear(), firstDayOfMonth.getMonth(), index + 1 - firstDayOfMonth.getDay())
+  let paddingDays = Array.from(
+    { length: firstDayOfMonth.getDay() },
+    (_, index) =>
+      new Date(
+        firstDayOfMonth.getFullYear(),
+        firstDayOfMonth.getMonth(),
+        index + 1 - firstDayOfMonth.getDay(),
+      ),
   );
 
   let allDays = [...paddingDays, ...daysOfMonth];
 
-
-  
-    const getDayClassName = (date: Date) => {
+  const getDayClassName = (date: Date) => {
     const dayOfWeek = date.getDay();
     if (dayOfWeek === 0) return 'sunday';
     if (dayOfWeek === 6) return 'saturday';
@@ -29,49 +38,40 @@ let DatabaseTable_Rows = () => {
     return '';
   };
 
- 
   const otherColumnData = [
-	 { format: (date: number | Date) => format(date, 'EEEE') }, // Định dạng ngày thành thứ
+    { format: (date: number | Date) => format(date, 'EEEE') }, // Định dạng ngày thành thứ
   ];
- 
 
-    // let rows = Array.from({ length: rowCount }, (_, rowIndex) => rowIndex + 1);
-    // let Addclass_Rows = ['', '', '', '', '', '', '', '', '', ''];
-    // let Addclass_Events = ['', 'sunday', 'holiday', 'accrept', 'cancel', 'saturday', '', '', '', ''];
+  // let rows = Array.from({ length: rowCount }, (_, rowIndex) => rowIndex + 1);
+  // let Addclass_Rows = ['', '', '', '', '', '', '', '', '', ''];
+  // let Addclass_Events = ['', 'sunday', 'holiday', 'accrept', 'cancel', 'saturday', '', '', '', ''];
 
-
-    const [showStartButton, setShowStartButton] = useState(true);
-   const [showEndButton, setShowEndButton] = useState(false);
+  const [showStartButton, setShowStartButton] = useState(true);
+  const [showEndButton, setShowEndButton] = useState(false);
 
   const handleEndButtonClick = () => {
     // Xử lý các tác vụ khi click vào nút kết thúc
     // Ví dụ: Ẩn nút kết thúc
-     setShowStartButton(false);
+    setShowStartButton(false);
     setShowEndButton(false);
   };
-	
-	 // State để lưu số giờ khi click vào nút
-	const [hours, setHours] = useState(0);
+
+  // State để lưu số giờ khi click vào nút
+  const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
 
-    const currentHour = new Date().getHours();
+  const currentHour = new Date().getHours();
   const currentMinutes = new Date().getMinutes();
-	// Hàm xử lý khi click vào nút
-	const handleButtonClick = () => {
-
-		setHours(currentHour);
+  // Hàm xử lý khi click vào nút
+  const handleButtonClick = () => {
+    setHours(currentHour);
     setMinutes(currentMinutes);
 
     setShowEndButton(true);
-     setShowStartButton(false);
+    setShowStartButton(false);
+  };
 
-     
-	};
-
-
- 
-
-    // State để theo dõi trạng thái mở rộng của các dòng
+  // State để theo dõi trạng thái mở rộng của các dòng
   const [expandedRows, setExpandedRows] = useState({});
 
   // State để lưu trữ ghi chú cho từng ngày
@@ -82,7 +82,7 @@ let DatabaseTable_Rows = () => {
   //   setExpandedRows((prevRows) => ({ ...prevRows, [rowIndex]: !prevRows[rowIndex] }));
 
   //   console.log("đã click ngày " + date);
-    
+
   //   // Nếu bạn có dữ liệu hiện tại của ghi chú, bạn có thể set nó cho biến noteContent
   //   // setNoteContent(getCurrentNoteForDate(date));
   // };
@@ -94,80 +94,107 @@ let DatabaseTable_Rows = () => {
   //   // saveNewNoteForDate(date, noteContent);
   // };
 
-
-
-    const isHoliday = (date: number | Date) => holidays.some((holiday) => isSameDay(date, holiday));
+  const isHoliday = (date: number | Date) =>
+    holidays.some((holiday) => isSameDay(date, holiday));
   const [holidays, setHolidays] = useState([
     // Đưa các ngày nghỉ mẫu vào đây, ví dụ:
     new Date(2023, 10, 1), // 1/12/2023
     // new Date(2023, 11, 15), // 15/12/2023
   ]);
 
-  const accreptLeave = (date: number | Date) => accreptLeaves.some((accrept) => isSameDay(date, accrept));
- const [accreptLeaves, setAccreptLeave] = useState([
+  const accreptLeave = (date: number | Date) =>
+    accreptLeaves.some((accrept) => isSameDay(date, accrept));
+  const [accreptLeaves, setAccreptLeave] = useState([
     // Đưa các ngày nghỉ mẫu vào đây, ví dụ:
     new Date(2023, 10, 15), // 1/12/2023
     // new Date(2023, 11, 15), // 15/12/2023
   ]);
 
-  const isCancelLeave = (date: number | Date) => cancelLeave.some((cancel) => isSameDay(date, cancel));
- const [cancelLeave, setCancelLeave] = useState([
+  const isCancelLeave = (date: number | Date) =>
+    cancelLeave.some((cancel) => isSameDay(date, cancel));
+  const [cancelLeave, setCancelLeave] = useState([
     // Đưa các ngày nghỉ mẫu vào đây, ví dụ:
     new Date(2023, 10, 22), // 1/12/2023
     // new Date(2023, 11, 15), // 15/12/2023
   ]);
-  
 
-
-    return(
-        <>
-    	 {allDays.map((day, rowIndex) => (
-            <tr key={rowIndex} className={`${getDayClassName(day)}${isToday(day) ? 'today' : '' } ${isHoliday(day) ? 'holiday' : ''} ${accreptLeave(day) ? 'accrept' : ''} ${isCancelLeave(day) ? 'cancel' : ''}`}>
-              {new Date(day).getMonth() === firstDayOfMonth.getMonth() ? (
-                <>
-                  <td>{format(day, 'dd/MM/yyyy')}</td>
-                  {otherColumnData.map((column, colIndex) => (
-					<td key={colIndex}>
-						{column.format ? column.format(day) : '...'} {/* Sử dụng hàm định dạng nếu có */}
-					</td>
-					))}
-					<td>
-            {isToday(day) && showStartButton  ?  <button className="btn btn--medium" onClick={handleButtonClick}>Bắt đầu</button> : ''}
-
-					</td>
-					<td>
-            {isToday(day) && showEndButton ?  <button className="btn btn--orange btn--medium" onClick={handleEndButtonClick} >Kết thúc</button> : ''}
-          </td>
-					<td> 8:00 </td>
-					<td> </td>
-					<td> </td>
-					<td> <a className="btn btn--green btn--small icon icon--edit"><img src={require('../../../assets/images/icnedit.png')}  alt="edit" className="fluid-image" /></a></td>
-					<td> </td>
-                </>
-              ) : null}
-            </tr>
-          ))}
-          <tr>
-            <td> Tổng số giờ</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td>72:00</td>
-            <td>00:00</td>
-            <td></td>
-            <td></td>
-          </tr>
-        </>
-    )
-} 
+  return (
+    <>
+      {allDays.map((day, rowIndex) => (
+        <tr
+          key={rowIndex}
+          className={`${getDayClassName(day)}${isToday(day) ? 'today' : ''} ${
+            isHoliday(day) ? 'holiday' : ''
+          } ${accreptLeave(day) ? 'accrept' : ''} ${
+            isCancelLeave(day) ? 'cancel' : ''
+          }`}
+        >
+          {new Date(day).getMonth() === firstDayOfMonth.getMonth() ? (
+            <>
+              <td>{format(day, 'dd/MM/yyyy')}</td>
+              {otherColumnData.map((column, colIndex) => (
+                <td key={colIndex}>
+                  {column.format ? column.format(day) : '...'}{' '}
+                  {/* Sử dụng hàm định dạng nếu có */}
+                </td>
+              ))}
+              <td>
+                {isToday(day) && showStartButton ? (
+                  <button
+                    className="btn btn--medium"
+                    onClick={handleButtonClick}
+                  >
+                    Bắt đầu
+                  </button>
+                ) : (
+                  ''
+                )}
+              </td>
+              <td>
+                {isToday(day) && showEndButton ? (
+                  <button
+                    className="btn btn--orange btn--medium"
+                    onClick={handleEndButtonClick}
+                  >
+                    Kết thúc
+                  </button>
+                ) : (
+                  ''
+                )}
+              </td>
+              <td> 8:00 </td>
+              <td> </td>
+              <td> </td>
+              <td>
+                {' '}
+                <a className="btn btn--green btn--small icon icon--edit">
+                  <img
+                    src={require('../../../assets/images/icnedit.png')}
+                    alt="edit"
+                    className="fluid-image"
+                  />
+                </a>
+              </td>
+              <td> </td>
+            </>
+          ) : null}
+        </tr>
+      ))}
+      <tr>
+        <td> Tổng số giờ</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>72:00</td>
+        <td>00:00</td>
+        <td></td>
+        <td></td>
+      </tr>
+    </>
+  );
+};
 
 export default DatabaseTable_Rows;
-
-
-
-
-
-
 
 /* cất code
 //  <>
@@ -178,7 +205,7 @@ export default DatabaseTable_Rows;
 //                     ))}
 //             </tr>
 //               ))}
-//         </>  
+//         </>
 
 
 
@@ -191,14 +218,14 @@ export default DatabaseTable_Rows;
   	const daysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
 	let current_day = currentDate.getDay();
 	const rows = [];
-	
-	
-	
+
+
+
 
 	for (let day = 1; day <= daysInMonth; day++) {
     const formattedDate = `${day}/${currentDate.getMonth() + 1}/${currentDate.getFullYear()}`;
 	const dayOfWeek = new Date(currentDate.getFullYear(), currentDate.getMonth(), day).toLocaleDateString('en-US', { weekday: 'long' });
-		
+
     rows.push(
 		<>
 		<tr key={day}>
@@ -212,7 +239,7 @@ export default DatabaseTable_Rows;
 			<td></td>
 			<td></td>
 		</tr>
-		
+
 	  </>
     );
   }
--- src/renderer/pages/Home/index.tsx
+++ src/renderer/pages/Home/index.tsx
@@ -1,7 +1,12 @@
+import { Button } from '../../components/Button';
+
 export const Home = () => {
   return (
     <div>
       <h2>Home page</h2>
+      {/* <Button color="orange" size="medium">
+        Login
+      </Button> */}
     </div>
   );
 };
