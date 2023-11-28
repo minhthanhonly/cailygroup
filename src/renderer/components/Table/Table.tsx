c6fb99834a86f8a5adbb7637b9e1031f4bec0c79
Caily.QuocThinh <quocthinh_web@caily.com.vn>
2023-11-28 08:26:08 +0700

    update and fix

--- src/renderer/components/Table/Table.tsx
+++ src/renderer/components/Table/Table.tsx
@@ -4,21 +4,13 @@
 import { SetStateAction, useState } from 'react';
 import DatabaseTable_Columns from './Table_01/DatabaseTable_Columns';
 import DatabaseTable_Rows from './Table_01/DatabaseTable_Rows';
-// import DatabaseTable02_Columns from './Table_02/DatabaseTable02_Columns';
-// import DatabaseTable02_Rows from './Table_02/DatabaseTable02_Rows';
-// import DatabaseTable03_Columns from './Table_03/DatabaseTable03_Columns';
-// import DatabaseTable03_Rows from './Table_03/DatabaseTable03_Rows';
 
-// import DatabaseTable04_Columns from './Table_04/DatabaseTable04_Columns';
-// import DatabaseTable04_Rows from './Table_04/DatabaseTable04_Rows';
-
-// import DatabaseTable05_Columns from './Table_05/DatabaseTable05_Columns';
-// import DatabaseTable05_Rows from './Table_05/DatabaseTable05_Rows';
 
 
 import CTable_Col from './TableCustom/CTable_Col';
 import CTable_Row from './TableCustom/CTable_Row';
 import MonthYearSelector from './SelectMonthYears';
+// import  TestOne  from "./TestOne";
 import './Table.scss';
 
 
@@ -52,71 +44,17 @@ const TablePage = () =>  {
 		<h2 className='hdg-lv2'>custom</h2>
         <div className='table-container'>
           <table className="table table__custom">
-          <thead>
-            <CTable_Col  Col_count={Col_count_rows} Col_title={Col_title} />
-          </thead>
-          <tbody>
-            {/* RowCounts = {RowCounts} */}
-            <CTable_Row Rows_count={Col_count_rows} />
-          </tbody>
+			<thead>
+				<CTable_Col  Col_count={Col_count_rows} Col_title={Col_title} />
+			</thead>
+			<tbody>
+				<CTable_Row Rows_count={Col_count_rows} />
+			</tbody>
        	 </table>
         </div>
 
-
-
-
- 
-        {/* <h2 className='hdg-lv2'>danh sách xin nghỉ phép - member</h2>
-         <div className='table-container table--02'>
-			<table className="table table__custom">
-				<thead>
-					<DatabaseTable02_Columns />
-				</thead>
-				<tbody>
-					{/* RowCounts = {RowCounts} 
-					<DatabaseTable02_Rows />
-				</tbody>
-        	</table>
-        </div>
-
-		<h2 className='hdg-lv2'>Danh sách duyệt nghỉ phép admin</h2>
-         <div className='table-container table--03'>
-			<table className="table table__custom">
-				<thead>
-					<DatabaseTable03_Columns />
-				</thead>
-				<tbody>
-					{/* RowCounts = {RowCounts} 
-					<DatabaseTable03_Rows />
-				</tbody>
-        	</table>
-        </div>
-
-		<h2 className='hdg-lv2'>danh sahcs thanh vien admin</h2>
-         <div className='table-container table--04'>
-			<table className="table table__custom">
-				<thead>
-					<DatabaseTable04_Columns />
-				</thead>
-				<tbody>
-					{/* RowCounts = {RowCounts} 
-					<DatabaseTable04_Rows />
-				</tbody>
-        	</table>
-        </div>
-
-		<h2 className='hdg-lv2'>Timecard-List admin</h2>
-         <div className='table-container table--05'>
-			<table className="table table__custom">
-				<thead>
-					<DatabaseTable05_Columns />
-				</thead>
-				<tbody>
-					{/* RowCounts = {RowCounts} 
-					<DatabaseTable05_Rows />
-				</tbody>
-        	</table>
-        </div> */}
+{/* 
+		<TestOne /> */}
     </div>
   );
 };
