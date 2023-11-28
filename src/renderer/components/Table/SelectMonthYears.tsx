0dad182b680c6b408641999ad24e11ea277a057e
Caily.QuocThinh <quocthinh_web@caily.com.vn>
2023-11-28 17:08:06 +0700

    update and fix

--- src/renderer/components/Table/SelectMonthYears.tsx
+++ src/renderer/components/Table/SelectMonthYears.tsx
@@ -1,63 +1,65 @@
- import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
+import { startOfMonth, endOfMonth, eachDayOfInterval, format } from 'date-fns';
 
-import './SelectMonthYears.scss'
- 
+interface MonthYearSelectorProps {
+  onChange: (selectedMonth: string, selectedYear: string, daysInMonth: Date[]) => void;
+}
 
-
-
-
-
- const MonthYearSelector = () => {
+const MonthYearSelector: React.FC<MonthYearSelectorProps> = ({ onChange }) => {
   const [selectedMonth, setSelectedMonth] = useState('');
   const [selectedYear, setSelectedYear] = useState('');
 
-  // Danh sách tháng và năm
   const months = Array.from({ length: 12 }, (_, index) => index + 1);
   const years = Array.from({ length: 10 }, (_, index) => new Date().getFullYear() - index);
 
   useEffect(() => {
-    // Lấy tháng và năm hiện tại
     const currentMonth = new Date().getMonth() + 1;
     const currentYear = new Date().getFullYear();
 
-    // Cập nhật giá trị ban đầu
     setSelectedMonth(currentMonth.toString());
     setSelectedYear(currentYear.toString());
-  }, []); // Chỉ chạy một lần khi component được render
+  }, []);
 
-  const handleMonthChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
-    setSelectedMonth(event.target.value);
+  const handleMonthChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value;
+    setSelectedMonth(value);
+    const daysInMonth = getDaysInMonth(selectedYear, value);
+    onChange(value, selectedYear, daysInMonth);
   };
 
-  const handleYearChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
-    setSelectedYear(event.target.value);
+  const handleYearChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value;
+    setSelectedYear(value);
+    const daysInMonth = getDaysInMonth(value, selectedMonth);
+    onChange(selectedMonth, value, daysInMonth);
+  };
+
+  const getDaysInMonth = (year: string, month: string) => {
+    const firstDayOfMonth = startOfMonth(new Date(parseInt(year), parseInt(month) - 1, 1));
+    const lastDayOfMonth = endOfMonth(new Date(parseInt(year), parseInt(month) - 1, 1));
+    return eachDayOfInterval({ start: firstDayOfMonth, end: lastDayOfMonth });
   };
 
   return (
     <div className='select__box'>
       <div className='select__box--flex grid-row'>
         <select value={selectedMonth} onChange={handleMonthChange}>
-            {months.map((month) => (
-              <option key={month} value={month}>{`Tháng ${month}`}</option>
-            ))}
-          </select>
-          <select value={selectedYear} onChange={handleYearChange}>
-            {years.map((year) => (
-              <option key={year} value={year}>{year}</option>
-            ))}
-      </select>
-
+          {months.map((month) => (
+            <option key={month} value={month}>
+              {`Tháng ${month}`}
+            </option>
+          ))}
+        </select>
+        <select value={selectedYear} onChange={handleYearChange}>
+          {years.map((year) => (
+            <option key={year} value={year}>
+              {year}
+            </option>
+          ))}
+        </select>
       </div>
-     
-
-      {/* Hiển thị giá trị được chọn */}
-      {/* <p>Tháng đã chọn: {selectedMonth}</p>
-      <p>Năm đã chọn: {selectedYear}</p> */}
     </div>
   );
 };
 
 export default MonthYearSelector;
-
-
-
