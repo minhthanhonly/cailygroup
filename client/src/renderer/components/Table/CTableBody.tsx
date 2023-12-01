b543865efd065a1f51a6e86b909c1fd6719dee2c
Caily.Tu <tu_caily@caily.com.vn>
2023-12-01 10:31:31 +0700

    Create Button Delete Component

--- client/src/renderer/components/Table/CTableBody.tsx
+++ client/src/renderer/components/Table/CTableBody.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import { ButtonDelete } from '../Button/ButtonDelete';
 
 interface TableRowProps {
   rowData: string[]; // Dữ liệu cho mỗi hàng
-  onButtonClick: () => void; // Hàm xử lý sự kiện khi button được click
+  onButtonClick?: () => void; // Hàm xử lý sự kiện khi button được click
+  admin?: boolean;
+  action: string;
 }
 
-let admin = true;
-
-const TableRow: React.FC<TableRowProps> = ({ rowData, onButtonClick }) => {
+const TableRow: React.FC<TableRowProps> = ({ rowData, onButtonClick, admin, action }) => {
   return (
     <tr>
       {rowData.map((data, index) => (
         <td key={index}>{data}</td>
       ))}
-      {admin == true ? <td>
-        <button onClick={onButtonClick}>Click me</button>
+      {admin == true && action == "delete"  ? <td>
+        <ButtonDelete onButtonClick={onButtonClick} />
       </td>
       :""}
     </tr>
@@ -23,13 +24,16 @@ const TableRow: React.FC<TableRowProps> = ({ rowData, onButtonClick }) => {
 
 interface TableProps {
   data: string[][]; // Dữ liệu cho bảng
+  admin?: boolean;
+  action: string;
 }
 
-const CTableBody: React.FC<TableProps> = ({ data }) => {
+const CTableBody: React.FC<TableProps> = ({ data, admin, action }) => {
   return (
       <tbody>
         {data.map((rowData, index) => (
-          <TableRow key={index} rowData={rowData} onButtonClick={() => console.log(`Button clicked for row ${index}`)} />
+          // <TableRow key={index} rowData={rowData} onButtonClick={() => console.log(`Button clicked for row ${index}`)} />
+          <TableRow key={index} rowData={rowData} admin={admin} action={action} />
         ))}
       </tbody>
   );
