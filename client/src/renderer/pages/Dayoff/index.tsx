3cfa249c3a23a1e9ba4636a6bc0b3bd4ef595f5f
Caily.Phuc <phuc_web@caily.com.vn>
2023-12-04 14:20:15 +0700

    up html

--- client/src/renderer/pages/Dayoff/index.tsx
+++ client/src/renderer/pages/Dayoff/index.tsx
@@ -4,7 +4,7 @@ import { Pagination } from "../../components/Pagination";
 import { CTable } from "../../components/Table/CTable";
 import CTableBody from "../../components/Table/CTableBody";
 import { CTableHead } from "../../components/Table/CTableHead";
-
+import { Link } from "react-router-dom";
 export const Dayoff = () => {
 
 const Data = [
@@ -16,6 +16,10 @@ const Data = [
     <>
 
       <Heading2 text="Danh sách xin nghỉ phép" />
+      <Link to="/day-off-list">Danh sách duyệt nghỉ phép</Link>
+      <br/>
+      <br/>
+      <Link to="/day-off-register">Đăng ký nghỉ phép</Link>
       <CTable>
         <CTableHead heads={["Họ Và Tên", "Số Ngày", "Ngày Bắt Đầu", "Ngày Kết Thúc", ""]}/>
         <CTableBody data={Data}/>
