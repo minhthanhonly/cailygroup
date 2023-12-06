e90d9ac3eb9f3da6bfdfcae7f7049e7852c9d38f
Caily.Phuc <phuc_web@caily.com.vn>
2023-12-06 14:36:15 +0700

    up html

--- client/src/renderer/components/Pagination/index.tsx
+++ client/src/renderer/components/Pagination/index.tsx
@@ -9,18 +9,10 @@ interface PaginationProps {
 
 
 export const Pagination = (Props: PaginationProps) => {
-
-
     const totalPages = Props.totalPages;
     const currentPage = Props.currentPage;
-
     const { onPageChange: onPageChange } = Props;
-
-
     const pageNumbers = Array.from({ length: totalPages }, (_, index) => index + 1);
-
-
-
     //
     const handleNextClick = () => {
         const nextPage = currentPage + 1;
@@ -33,18 +25,11 @@ export const Pagination = (Props: PaginationProps) => {
         // Nếu đang ở trang cuối cùng, chuyển về trang đầu tiên
         onPageChange(PreviousPage < totalPages ? 1 : PreviousPage);
     };
-
-
-    console.log("totalPages", totalPages);
-    console.log("currentPage", currentPage);
-    console.log("currentPage + 1", currentPage + 1);
-
     return (
         <ul className='list-page'>
             {totalPages === 1 || currentPage === 1 ? "" : <> <li>
                 <a href="#" onClick={() => handlePreviousClick()}>Previous</a>
             </li> </>}
-
             {pageNumbers.map((pageNumber) => (
                 <li key={pageNumber} className={pageNumber === currentPage ? 'active' : ''}>
                     <a href="#" onClick={() => onPageChange(pageNumber)}>
