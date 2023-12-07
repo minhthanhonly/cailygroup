ddccfdb93619ffdade8af133c6b21d53ebe9295a
Caily.Tu <tu_caily@caily.com.vn>
2023-12-07 17:01:43 +0700

    Update

--- client/src/renderer/components/Table/CTableBody.tsx
+++ client/src/renderer/components/Table/CTableBody.tsx
@@ -4,7 +4,9 @@ import { Button } from '../Button';
 import { Link } from 'react-router-dom';
 
 interface TableRowProps {
-  rowData: string[]; // Dữ liệu cho mỗi hàng
+  rowData: {
+    [key: string]: any;
+  }; // Dữ liệu cho mỗi hàng
   onButtonClick?: () => void; // Hàm xử lý sự kiện khi button được click
   admin?: boolean;
   deleteData?: boolean;
@@ -12,25 +14,15 @@ interface TableRowProps {
   exportData?: boolean;
   editData?: boolean;
   accreptAdminData?: boolean; // đồng ý
-  // refuseData? : boolean; // hủy
-  // accreptMemberView ?: boolean;
 }
-
-
 
 const TableRow: React.FC<TableRowProps> = ({ rowData, onButtonClick, admin, deleteData, showData, exportData, editData, accreptAdminData, }) => {
 
-
-
-
-  // const filteredRowData = rowData.filter((item, index) => !(index === rowData.length - 1 && (item === '0' || item === '1')));
-
-  // const rowDataIndexLast = rowData[rowData.length - 1];
-
+  const valuesRowData: any[] = Object.values(rowData);
 
   return (
     <tr>
-      {rowData.map((data, index) => (
+      {valuesRowData.map((data, index) => (
         <td key={index}>{data}</td>
       ))}
 
@@ -50,46 +42,6 @@ const TableRow: React.FC<TableRowProps> = ({ rowData, onButtonClick, admin, dele
         <Button href="/" size='medium' color="green">Xác Nhận</Button>
       </td>
         : ""}
-      {/* {refuseData == true ? <td>
-        <Button href="/" size='medium' color="orange">Hủy</Button>
-      </td>
-        : "" } */}
-
-      {/* {rowDataIndexLast === '0' ? <td>
-          <p className="icon icon--check">
-            <img
-              src={require('../../assets/images/check.png')}
-              alt="edit"
-              className="fluid-image"
-            />
-          </p>
-      </td> : <td>
-        <Button href="/" size='medium' color="orange">Hủy</Button>
-      </td> } */}
-      {/* {rowDataIndexLast === '1' ? <td>
-        <p className="icon icon--check">
-          <img
-            src={require('../../assets/images/check.png')}
-            alt="edit"
-            className="fluid-image"
-          />
-        </p>
-      </td> : <td>
-        <Button href="/" size='medium' color="orange">Hủy</Button>
-      </td>} */}
-
-      {/* {accreptMemberView == true ?  <td>
-          <p className="icon icon--check">
-            <img
-              src={require('../../assets/images/check.png')}
-              alt="edit"
-              className="fluid-image"
-            />
-          </p>
-      </td>
-        : ""  } */}
-
-
       {editData == true ? <td>
         <div className="grid-row icon-flex">
           <Link to={'/member/edit'}>
@@ -116,22 +68,22 @@ const TableRow: React.FC<TableRowProps> = ({ rowData, onButtonClick, admin, dele
 };
 
 interface TableProps {
-  data: string[][]; // Dữ liệu cho bảng
+  data: {
+    [key: string]: any;
+  }[]; // Dữ liệu cho bảng
   admin?: boolean;
   deleteData?: boolean;
   showData?: boolean;
   exportData?: boolean;
   editData?: boolean;
   accreptAdminData?: boolean; // đồng ý
-  // refuseData? : boolean; // huy bo
-  // accreptMemberView? :boolean;
 }
-// refuseData , accreptMemberView
+
 const CTableBody: React.FC<TableProps> = ({ data, admin, deleteData, showData, exportData, editData, accreptAdminData, }) => {
+
   return (
     <tbody>
       {data.map((rowData, index) => (
-        // <TableRow key={index} rowData={rowData} onButtonClick={() => console.log(`Button clicked for row ${index}`)} />
         <TableRow key={index} rowData={rowData} admin={admin} deleteData={deleteData} showData={showData} exportData={exportData} editData={editData} accreptAdminData={accreptAdminData} />
       ))}
     </tbody>
--- client/src/renderer/pages/Group/index.tsx
+++ client/src/renderer/pages/Group/index.tsx
@@ -1,5 +1,50 @@
+import { useEffect, useState } from 'react';
 import { TableGroup } from '../../components/Table/TableGroup';
+import axios from 'axios';
+import { CTable } from '../../components/Table/CTable';
+import { CTableHead } from '../../components/Table/CTableHead';
+import CTableBody from '../../components/Table/CTableBody';
+
 
 export const Group = () => {
-  return <TableGroup />;
+
+  type FieldGroups = {
+    group_name:string;
+  }
+  const [listOfGroups, setListOfGroups] = useState<FieldGroups[] | []>([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/groups").then((response) => {
+      setListOfGroups(response.data)
+      console.log(response.data);
+    })
+  }, [])
+
+  // let items: FieldGroups[] = [];
+
+  // for (let i = 0; i < listOfGroups.length; i++) {
+  //   // Tạo một đối tượng mới và thêm vào mảng
+  //   items.push({ group_name: `${listOfGroups.group_name}`});
+  // }
+
+
+  // {listOfGroups.map((value, key) => {
+  //   names.push(value.group_name)
+  // })}
+  // names.push()
+
+  // console.log(names);
+
+  return (
+    <>
+      <CTable>
+        <CTableHead heads={["STT", "Tên Nhóm", "Hành Động"]}/>
+        <CTableBody data={listOfGroups}/>
+      </CTable>
+
+
+    </>
+  )
+
+  // <TableGroup />;
 };
