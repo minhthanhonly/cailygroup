33080187985418fa4b8076b50ed471ef8f3c9263
Caily.QuocThinh <quocthinh_web@caily.com.vn>
2023-12-20 14:16:18 +0700

    update

--- client/src/renderer/pages/Group/index.tsx
+++ client/src/renderer/pages/Group/index.tsx
@@ -8,34 +8,34 @@ import { Heading2 } from '../../components/Heading';
 import { urlControl } from '../../routes/server';
 
 export const Group = () => {
-  type FieldGroups = {
-    id: string,
-    group_name:string;
-  }
-  const [listOfGroups, setListOfGroups] = useState<FieldGroups[] | []>([]);
+	type FieldGroups = {
+		id: string,
+		group_name: string;
+	}
+	const [listOfGroups, setListOfGroups] = useState<FieldGroups[] | []>([]);
 
-  useEffect(() => {
-    axios.get(urlControl + "GroupsController.php").then((response) => {
-      setListOfGroups(response.data)
-    })
-  }, [])
+	useEffect(() => {
+		axios.get(urlControl + "GroupsController.php").then((response) => {
+			setListOfGroups(response.data)
+		})
+	}, [])
 
-  let DataTable: FieldGroups[] = [];
-  for (let i = 0; i < listOfGroups.length; i++) {
-    DataTable.push({
-      id: `${listOfGroups[i].id}`,
-      group_name: `${listOfGroups[i].group_name}`
-    });
-  }
+	let DataTable: FieldGroups[] = [];
+	for (let i = 0; i < listOfGroups.length; i++) {
+		DataTable.push({
+			id: `${listOfGroups[i].id}`,
+			group_name: `${listOfGroups[i].group_name}`
+		});
+	}
 
-  return (
-    <>
-      <Heading2 text="Quản lý nhóm" />
-      <AddGroup />
-      <CTable>
-        <CTableHead heads={["STT", "Tên Nhóm", "Sửa", "Xóa"]}/>
-        <CTableBody data={DataTable} permission_edit={true} path_edit='/group/edit' permission_delete={true} />
-      </CTable>
-    </>
-  )
+	return (
+		<>
+			<Heading2 text="Quản lý nhóm" />
+			<AddGroup />
+			<CTable>
+				<CTableHead heads={["STT", "Tên Nhóm", "Sửa", "Xóa"]} />
+				<CTableBody data={DataTable} permission_edit={true} path_edit='/group/edit' permission_delete={true} />
+			</CTable>
+		</>
+	)
 };
