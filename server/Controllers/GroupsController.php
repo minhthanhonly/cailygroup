f119929e7cf2246cbcd3d132dff511bd0202887d
Caily.Thuy <thuy_web@caily.com.vn>
2023-12-22 17:01:35 +0700

    up html

--- server/Controllers/GroupsController.php
+++ server/Controllers/GroupsController.php
@@ -3,7 +3,7 @@ require('../database/DBConnect.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: POST, GET, OPTIONS,DELETE");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS,DELETE,PUT");
     header("Access-Control-Allow-Headers: Content-Type");
     http_response_code(200);
     exit;
@@ -61,10 +61,27 @@ switch($method) {
             echo json_encode(["error" => "Dữ liệu không hợp lệ. 'group_name' bị thiếu"]);
         }
     break;
-
+    
 
     case "PUT":
-
+        $data = json_decode(file_get_contents("php://input"), true);
+        if (isset($data['id'], $data['group_name'])) {
+            $id = mysqli_real_escape_string($db_conn, $data['id']);
+            $group_name = mysqli_real_escape_string($db_conn, $data['group_name']);
+    
+            $updateQuery = "UPDATE groups SET group_name = '$group_name' WHERE id = '$id'"; 
+    
+            if (mysqli_query($db_conn, $updateQuery)) {
+                http_response_code(200);
+                echo json_encode(["message" => "Data update successfully"]);
+            } else {
+                http_response_code(500);
+                echo json_encode(["error" => "Failed to update data"]);
+            }
+        } else {
+            http_response_code(400);
+            echo json_encode(["error" => "Invalid data"]);
+        }
         break;
         
     case "DELETE":
