4c4131a05cd2bbcd77408657c611c8a1a2487aa7
Caily.Tu <tu_caily@caily.com.vn>
2024-01-08 09:28:11 +0700

    Update edit user

--- server/app/models/UsersModel.php
+++ server/app/models/UsersModel.php
@@ -103,7 +103,7 @@
 			$conn->close();
 		}
 
-		function postUpdate($userid, $password, $realname, $authority, $user_group){
+		function postUpdate($userid, $password, $passwordNew, $realname, $authority, $user_group){
 			global $conn;
 			
 			if ($_SERVER["REQUEST_METHOD"] === "POST") {
@@ -112,13 +112,22 @@
 				// Lấy dữ liệu từ phần thân của yêu cầu
 				$id = $userPostData->id;
 				$userid = $userPostData->userid;
-				$password = password_hash($userPostData->password, PASSWORD_BCRYPT);
+				$password = $userPostData->password;
+				$passwordNew = $userPostData->passwordNew;
+				if(!empty($passwordNew)) {
+					$passwordNew = password_hash($userPostData->passwordNew, PASSWORD_BCRYPT);
+				}
 				$realname = $userPostData->realname;
 				$authority = $userPostData->authority;
 				$user_group = $userPostData->user_group;
 			
 				// Cập nhật dữ liệu vào cơ sở dữ liệu
-				$sql = "UPDATE users SET userid='$userid', password='$password', password_default='', realname='$realname', authority='$authority', user_group='$user_group', user_groupname='', user_email='', user_skype='', user_ruby='', user_postcode='',user_address='', user_addressruby='', user_phone='', user_mobile='', user_order='', edit_level='', edit_group='', edit_user='', owner='', editor='', createdAt='', updatedAt='' WHERE id='$id'";
+				if(!empty($passwordNew)) {
+					$sql = "UPDATE users SET userid='$userid', password='$passwordNew', password_default='', realname='$realname', authority='$authority', user_group='$user_group', user_groupname='', user_email='', user_skype='', user_ruby='', user_postcode='',user_address='', user_addressruby='', user_phone='', user_mobile='', user_order='', edit_level='', edit_group='', edit_user='', owner='', editor='', createdAt='', updatedAt='' WHERE id='$id'";
+				} else {
+					$sql = "UPDATE users SET userid='$userid', password='$password', password_default='', realname='$realname', authority='$authority', user_group='$user_group', user_groupname='', user_email='', user_skype='', user_ruby='', user_postcode='',user_address='', user_addressruby='', user_phone='', user_mobile='', user_order='', edit_level='', edit_group='', edit_user='', owner='', editor='', createdAt='', updatedAt='' WHERE id='$id'";
+				}
+
 				$result = $conn->query($sql);
 
 				header('Content-Type: application/json');
