ebc200677e3c578f7ff6d1041eb7891a9b1ef0a7
Caily.QuocThinh <quocthinh_web@caily.com.vn>
2024-01-09 13:36:05 +0700

    update MVC

--- server/app/controllers/Timecards.php
+++ server/app/controllers/Timecards.php
@@ -6,5 +6,9 @@
         function add($user_id='', $timecard_year='', $timecard_month='', $timecard_day='', $timecard_date='', $owner='', $timecard_temp=''){
             return $this->model('TimecardsModel')->postAdd($user_id, $timecard_year, $timecard_month, $timecard_day, $timecard_date, $owner, $timecard_temp);
         }
+
+        function list(){
+            return $this->model('TimecardsList')->getList();
+        }
     }
 ?>
