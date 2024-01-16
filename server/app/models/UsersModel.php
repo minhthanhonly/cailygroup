4aec9be633d3c7d66d0871c56e2c5ef514e49957
Caily.Tu <tu_caily@caily.com.vn>
2024-01-16 17:00:58 +0700

    Fix

--- server/app/models/UsersModel.php
+++ server/app/models/UsersModel.php
@@ -144,33 +144,32 @@
 				$id = $userPostData->id;
 				$userid = $userPostData->userid;
 				$password = $userPostData->password;
-				$passwordNew = $userPostData->passwordNew;
+				// $passwordNew = $userPostData->passwordNew;
 
-				var_dump($userPostData);
-				// if(!empty($passwordNew)) {
-				// 	$passwordNew = password_hash($userPostData->passwordNew, PASSWORD_BCRYPT);
-				// }
-				// $realname = $userPostData->realname;
-				// $authority = $userPostData->authority;
-				// $user_group = $userPostData->user_group;
+				if(!empty($passwordNew)) {
+					$passwordNew = password_hash($userPostData->passwordNew, PASSWORD_BCRYPT);
+				}
+				$realname = $userPostData->realname;
+				$authority = $userPostData->authority;
+				$user_group = $userPostData->user_group;
 			
-				// // Cập nhật dữ liệu vào cơ sở dữ liệu
-				// if(!empty($passwordNew)) {
-				// 	$sql = "UPDATE users SET userid='$userid', password='$passwordNew', password_default='', realname='$realname', authority='$authority', user_group='$user_group', user_groupname='', user_email='', user_skype='', user_ruby='', user_postcode='',user_address='', user_addressruby='', user_phone='', user_mobile='', user_order='', edit_level='', edit_group='', edit_user='', owner='', editor='', createdAt='', updatedAt='' WHERE id='$id'";
-				// } else {
-				// 	$sql = "UPDATE users SET userid='$userid', password='$password', password_default='', realname='$realname', authority='$authority', user_group='$user_group', user_groupname='', user_email='', user_skype='', user_ruby='', user_postcode='',user_address='', user_addressruby='', user_phone='', user_mobile='', user_order='', edit_level='', edit_group='', edit_user='', owner='', editor='', createdAt='', updatedAt='' WHERE id='$id'";
-				// }
+				// Cập nhật dữ liệu vào cơ sở dữ liệu
+				if(!empty($passwordNew)) {
+					$sql = "UPDATE users SET userid='$userid', password='$passwordNew', password_default='', realname='$realname', authority='$authority', user_group='$user_group', user_groupname='', user_email='', user_skype='', user_ruby='', user_postcode='',user_address='', user_addressruby='', user_phone='', user_mobile='', user_order='', edit_level='', edit_group='', edit_user='', owner='', editor='', createdAt='', updatedAt='' WHERE id='$id'";
+				} else {
+					$sql = "UPDATE users SET userid='$userid', password='$password', password_default='', realname='$realname', authority='$authority', user_group='$user_group', user_groupname='', user_email='', user_skype='', user_ruby='', user_postcode='',user_address='', user_addressruby='', user_phone='', user_mobile='', user_order='', edit_level='', edit_group='', edit_user='', owner='', editor='', createdAt='', updatedAt='' WHERE id='$id'";
+				}
 
-				// $result = $conn->query($sql);
+				$result = $conn->query($sql);
 
-				// header('Content-Type: application/json');
-				// if($result) {
-				// 	echo json_encode(['success' => 'Cập nhật thành viên thành công']);
-				// 	return;
-				// } else {
-				// 	echo json_encode(['success' => 'Please check the Users data!']);
-				// 	return;
-				// }
+				header('Content-Type: application/json');
+				if($result) {
+					echo json_encode(['success' => 'Cập nhật thành viên thành công']);
+					return;
+				} else {
+					echo json_encode(['success' => 'Please check the Users data!']);
+					return;
+				}
 			}
 		}
 
