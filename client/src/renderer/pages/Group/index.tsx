76bc1c64490f12b5445ab0abc1a7fba5a9e59928
Caily.Thuy <thuy_web@caily.com.vn>
2024-01-17 15:03:03 +0700

    update

--- client/src/renderer/pages/Group/index.tsx
+++ client/src/renderer/pages/Group/index.tsx
@@ -6,7 +6,7 @@ import CTableBody from '../../components/Table/CTableBody';
 import { Heading2 } from '../../components/Heading';
 import Modal from '../../components/Modal/Modal';
 import Modaldelete from '../../components/Modal/Modaldelete';
-import {isValidUser} from "../../components/Validate";
+import {isValidGroup} from "../../components/Validate";
 
 interface GroupProps {
   id: string;
@@ -40,7 +40,7 @@ export const Group = () => {
   const [listOfUsers, setListOfUsers] = useState<FieldUsers[] | []>([]);
   const [isDisabled, setIsDisabled] = useState<boolean>(true);
   const [selectedGroup, setSelectedGroup] = useState('');
-  const [formValue, setFormValue] = useState({ group_name: '' })
+  
   const fetchMembersByGroup = async($groupid: string) => {
     const groupid = {groupid:$groupid};
     const res = await axios.get("users/groups/"+$groupid);
@@ -126,18 +126,18 @@ export const Group = () => {
   };
 
   const handleUpdate = async (id: string, group_name: string, event:any) => {
-    const validationErrors = isValidUser({...formValue});
+    const validationErrors = isValidGroup({group_name});
     if (event) {
-      event.preventDefault();
-      try {
-        const dataUpdate = { id, group_name };
-        const response = await axios.put('groups/update/',dataUpdate,{ headers: { 'Content-Type': 'application/json' } });
-        console.log('Update Response:', response.data);
-        closeModal();
-        setIsTableUpdated(true); //Khi thêm nhóm mới ,cập nhật state mới
-      } catch (error) {
-        console.error('Lỗi khi cập nhật trạng thái:', error);
-      }
+        event.preventDefault();
+        if(validationErrors === true) {
+          try {
+            const dataUpdate = { id, group_name };
+            const response = await axios.put('groups/update/',dataUpdate,{ headers: { 'Content-Type': 'application/json' } });
+            console.log('Update Response:', response.data);
+            closeModal();
+            setIsTableUpdated(true); //Khi thêm nhóm mới ,cập nhật state mới
+          } catch (error) { console.error('Lỗi khi cập nhật trạng thái:', error); }
+        }
     }
   };
 
