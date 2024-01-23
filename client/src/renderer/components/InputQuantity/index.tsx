cee23678f5fccfff0094f31b13a69a3a68d8defd
Caily.QuocThinh <quocthinh_web@caily.com.vn>
2024-01-23 10:48:06 +0700

    update

--- client/src/renderer/components/InputQuantity/index.tsx
+++ client/src/renderer/components/InputQuantity/index.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useState, KeyboardEvent } from 'react';
 
 import './InputQuantity.scss';
 
@@ -8,19 +8,45 @@ type InputQuantityProps = {
 };
 
 export const InputQuantity = (props: InputQuantityProps) => {
-  const [inputValue, setInputValue] = useState('5');
+  const [inputValue, setInputValue] = useState('2');
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
+    let value = event.target.value;
+
+    // Prevent entering '0'
+    if (value === '0' || value === '00') {
+      value = '2';
+    }
+
     setInputValue(value);
     props.onItemsPerPageChange(Number(value));
   };
+
+  const handleKeyPress = (event: KeyboardEvent<HTMLInputElement>) => {
+    // Reset to '1' when Backspace or Delete key is pressed
+    if (event.key === 'Backspace' || event.key === 'Delete') {
+      setInputValue('2');
+      props.onItemsPerPageChange(2);
+      event.preventDefault();
+    }
+  };
+
+  // Ensure itemsPerPage is greater than 0
+  const itemsPerPage = Math.max(1, Number(inputValue));
+
+  // Calculate totalPages
+  const totalPages = Math.ceil(props.total / itemsPerPage);
 
   return (
     <span className="title-quantity">
       Số Lượng Hiển Thị{' '}
       <span className="input-show">
-        <input value={inputValue} onChange={handleInputChange} type="text" />
+        <input
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyPress}
+          type="text"
+        />
       </span>
     </span>
   );
