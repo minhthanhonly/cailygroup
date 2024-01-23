36602fa22933173537da35472c3e711c3e0a5b5a
Caily.QuocThinh <quocthinh_web@caily.com.vn>
2024-01-23 10:38:00 +0700

    update phân trang user

--- client/src/renderer/components/InputQuantity/index.tsx
+++ client/src/renderer/components/InputQuantity/index.tsx
@@ -1,10 +1,27 @@
+import React, { ChangeEvent, useState } from 'react';
+
 import './InputQuantity.scss';
 
 type InputQuantityProps = {
   total: number;
-}
+  onItemsPerPageChange: (value: number) => void;
+};
+
 export const InputQuantity = (props: InputQuantityProps) => {
-    return (
-        <span className="title-quantity">Số Lượng Hiển Thị <span className='input-show'><input value={props.total} onChange={(event) => (event.target.value)} type="text" /></span></span>
-    );
+  const [inputValue, setInputValue] = useState('5');
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setInputValue(value);
+    props.onItemsPerPageChange(Number(value));
+  };
+
+  return (
+    <span className="title-quantity">
+      Số Lượng Hiển Thị{' '}
+      <span className="input-show">
+        <input value={inputValue} onChange={handleInputChange} type="text" />
+      </span>
+    </span>
+  );
 };
