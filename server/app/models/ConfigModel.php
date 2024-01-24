d295e3f34ef542b437052fe0627fea6b69154ebc
Hieu_web <hieu_web@caily.com.vn>
2024-01-24 08:47:19 +0700

    update mã lỗi

--- server/app/models/ConfigModel.php
+++ server/app/models/ConfigModel.php
@@ -3,16 +3,11 @@
         function getConfig(){
             global $conn;
             $allGroup = mysqli_query($conn, "SELECT * FROM configs");
-            if(mysqli_num_rows($allGroup) > 0) {
-                while ($row = mysqli_fetch_array($allGroup)) {
-                    $json_array["configdata"][] = $row;
-                }
-                echo json_encode($json_array["configdata"]);
-                return;
-            } else {
-                echo json_encode(["result" => "Please check the Data"]);
-                return;
+            while ($row = mysqli_fetch_array($allGroup)) {
+                $json_array["configdata"][] = $row;
             }
+            echo json_encode($json_array["configdata"]);
+            return;
             $conn->close();
         }
     }
