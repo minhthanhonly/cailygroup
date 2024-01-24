a15e2601ea5ac133103c3fbb025ef4dfa67cf2b8
Hieu_web <hieu_web@caily.com.vn>
2024-01-24 08:04:55 +0700

    update controller

--- server/app/controllers/Config.php
+++ server/app/controllers/Config.php
@@ -1,7 +1,15 @@
 <?php
     class Config extends Controller{
+        function __construct(){
+            $getAuthHeader = Controller::getAuthorizationHeader();
+            $getBearerToken = Controller::getBearerToken($getAuthHeader);
+            return $getBearerToken;
+        }
         function index(){
-            $dataConfig = $this->model('ConfigModel')->getConfig();
+            $is_jwt_valid = Controller::is_jwt_valid($this->__construct());
+            if($is_jwt_valid == 1){
+                $dataConfig = $this->model('ConfigModel')->getConfig();
+            }
         }
     }
 ?>
