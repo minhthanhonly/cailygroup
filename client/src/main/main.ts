b4e8988771b7a5e4e69c2ff496c89ef9dbd9f877
Caily.Tu <tu_caily@caily.com.vn>
2024-01-30 09:35:04 +0700

    add app version

--- client/src/main/main.ts
+++ client/src/main/main.ts
@@ -80,6 +80,9 @@ const createWindow = async () => {
 
   mainWindow.loadURL(resolveHtmlPath('index.html'));
 
+  // Lấy phiên bản ứng dụng
+  const appVersion = app.getVersion();
+
   mainWindow.on('ready-to-show', () => {
     if (!mainWindow) {
       throw new Error('"mainWindow" is not defined');
@@ -93,6 +96,12 @@ const createWindow = async () => {
 
   mainWindow.on('closed', () => {
     mainWindow = null;
+  });
+
+  // Gán phiên bản vào phần tử HTML
+  mainWindow.webContents.on('did-finish-load', () => {
+    mainWindow?.webContents.executeJavaScript(`localStorage.setItem('app-version', '${appVersion}');`);
+    mainWindow?.webContents.executeJavaScript(`document.getElementById('app-version').innerHTML = 'Phiên bản: ${appVersion}';`);
   });
 
   const menuBuilder = new MenuBuilder(mainWindow);
