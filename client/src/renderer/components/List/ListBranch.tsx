7eb500f5899dec0a5b2a8cc4569505a0ad9f25c0
Caily.Tu <tu_caily@caily.com.vn>
2024-02-02 15:34:39 +0700

    no message

--- client/src/renderer/components/List/ListBranch.tsx
+++ client/src/renderer/components/List/ListBranch.tsx
@@ -1,4 +1,5 @@
-import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 interface ListBranchProps {
   branch: {
@@ -8,11 +9,26 @@ interface ListBranchProps {
 }
 
 const ListBranch: React.FC<ListBranchProps> = ({ branch }) => {
+  // const [activeTab, setActiveTab] = useState<number>(() => {
+  //   const storedTab = localStorage.getItem('activeTab');
+  //   return storedTab ? parseInt(storedTab, 10) : 0;
+  // });
+
+  // const handleTabClick = (tabId: number) => {
+  //   setActiveTab(tabId);
+  //   localStorage.setItem('activeTab', tabId.toString());
+  // };
+
+  // useEffect(() => {
+  //   localStorage.setItem('activeTab', activeTab.toString());
+  // }, [activeTab]);
+
   return (
     <>
       <ul className="lst-branch">
-        {branch.map((item) => (
-          <li key={item.text}><NavLink to={item.to}>{item.text}</NavLink></li>
+        {branch.map((item, index) => (
+          // <li key={index}><Link to={item.to} onClick={() => handleTabClick(index)} className={`tab ${index === activeTab ? 'active' : ''}`}>{item.text}</Link></li>
+          <li key={index}><NavLink to={item.to}>{item.text}</NavLink></li>
         ))}
       </ul>
     </>
