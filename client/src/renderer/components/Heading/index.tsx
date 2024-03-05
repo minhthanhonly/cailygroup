12a935d322b76738202e62296c4a5fc9ffec4d26
Caily.Thanh <thanhonly@caily.com.vn>
2024-03-05 13:40:10 +0700

    update

--- client/src/renderer/components/Heading/index.tsx
+++ client/src/renderer/components/Heading/index.tsx
@@ -11,6 +11,13 @@ export const Heading2 = (props : attr) => {
         <h2 className="hdglv2"><span>{text_title}</span></h2>
     );
 };
+export const HeadingLV2 = (props : attr) => {
+
+    let text_title = props.text;
+    return (
+        <h2 className="hdg-lv2"><span>{text_title}</span></h2>
+    );
+};
 
 export const Heading3 = (props : attr) => {
     let text_title = props.text;
