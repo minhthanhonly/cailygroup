dd8f2c747e497caf09fb207e4528e11a90d29c72
Caily.Thuy <thuy_web@caily.com.vn>
2024-05-09 16:54:16 +0700

    up html

--- server/app/models/ApplicationModel.php
+++ server/app/models/ApplicationModel.php
@@ -1,29 +1,6 @@
 <?php
     class ApplicationModel{
 
-        // function getApplication(){
-        //     global $conn;
-        //     $statusFilter = isset($_GET['id_status']) ? mysqli_real_escape_string($conn, $_GET['id_status']) : '-1';
-        //     $sql = "SELECT * FROM table_json";
-        //     if ($statusFilter != -1) {
-        //         $sql .= " WHERE id_status = $statusFilter"; // Sửa từ $query thành $sql
-        //     }
-        //    // echo $sql; // In ra câu truy vấn để kiểm tra
-        // //    $sql .= " ORDER BY id DESC"; 
-        //     $result = $conn->query($sql);
-        //     $data = array();
-        //     if ($result->num_rows > 0) {
-        //        while ($row = $result->fetch_assoc()) {
-        //             $data[] = $row;
-        //         }
-        //     }
-        //     // Trả về dữ liệu dưới dạng JSON
-        //     header('Content-Type: application/json');
-        //     echo json_encode($data);
-        //     return;
-        //     $conn->close();
-        // }
-
         function getApplication(){
             global $conn;
             $statusFilter = isset($_GET['id_status']) ? mysqli_real_escape_string($conn, $_GET['id_status']) : '-1';
@@ -66,7 +43,6 @@
 
         function getApplicationByIdStatus($idStatus){
             global $conn;
-
             $statusFilter = isset($_GET['id_status']) ? mysqli_real_escape_string($conn, $_GET['id_status']) : '-1';
             $sql = "SELECT * FROM application_details WHERE id_status = '$idStatus'";
             $result = $conn->query($sql);
@@ -83,25 +59,6 @@
             return;
         }
 
-        // function getApplicationForId($id){
-        //     global $conn;
-        //     $sql = "SELECT *
-        //             FROM table_json 
-        //             WHERE table_json.id = $id";
-        //     $result = $conn->query($sql);
-        //     $register = null;
-
-        //     if ($result && $result->num_rows > 0) {
-        //         $register = $result->fetch_assoc(); // Lấy ra đối tượng từ kết quả truy vấn
-        //     } else {
-        //         // Nếu không có dòng nào khớp với id, trả về một đối tượng trống
-        //         $register = new stdClass();
-        //     }
-
-        //     echo json_encode($register); // Trả về đối tượng JSON
-        //     $conn->close();
-        // }
-
         function getApplicationForId($id){
             global $conn;
             $sql = "SELECT *
@@ -109,14 +66,12 @@
                     WHERE application_details.id = $id";
             $result = $conn->query($sql);
             $register = null;
-
             if ($result && $result->num_rows > 0) {
                 $register = $result->fetch_assoc(); // Lấy ra đối tượng từ kết quả truy vấn
             } else {
                 // Nếu không có dòng nào khớp với id, trả về một đối tượng trống
                 $register = new stdClass();
             }
-
             echo json_encode($register); // Trả về đối tượng JSON
             $conn->close();
         }
@@ -174,82 +129,6 @@
             }
         }
 
-        // function updateStatus($id, $id_status) {
-        //     global $conn;
-        //     if ($_SERVER["REQUEST_METHOD"] === "PUT") {
-        //         header('Content-Type: application/json'); // Di chuyển câu lệnh này lên đầu
-                
-        //         $groupUpdateData = json_decode(file_get_contents("php://input"), true);
-        //         if (isset($groupUpdateData['id'], $groupUpdateData['id_status'])) {
-        //             $id = mysqli_real_escape_string($conn, $groupUpdateData['id']);
-        //             $id_status = mysqli_real_escape_string($conn, $groupUpdateData['id_status']);
-        //             mysqli_query($conn, "SET time_zone = '+07:00'");
-        //             // Lấy dữ liệu JSON từ cột tablejson
-        //             $selectQuery = "SELECT JSON_EXTRACT(tablejson, '$.id_status') AS id_status FROM table_json WHERE id = '$id'";
-        //             $results = mysqli_query($conn, $selectQuery);
-        //             $row = mysqli_fetch_assoc($results);
-                           
-        //             $current_json = json_decode($row['id_status'], true);
-        //             if (isset($current_json)) {
-        //                 $current_json = $id_status;
-        //                 $new_json = json_encode($current_json);
-        //                 $updateQuery = "UPDATE table_json SET tablejson = JSON_SET(tablejson, '$.id_status', $new_json), id_status = '$id_status', createdAt = NOW() WHERE id ='$id'";
-                
-        //                 if (mysqli_query($conn, $updateQuery)) {
-        //                     $responseData = ["message" => "Dữ liệu được cập nhật thành công"];
-        //                 } else {
-        //                     http_response_code(500);
-        //                     $responseData = ["error" => "Không thể cập nhật dữ liệu"];
-        //                 }
-                        
-        //                 $sqlSelect = "SELECT * FROM table_json WHERE id = '$id'";
-        //                 $result = $conn->query($sqlSelect);
-        //                 if ($result->num_rows > 0) {
-        //                     while ($row = $result->fetch_assoc()) {
-        //                         $data = $row;
-        //                     }
-                        
-        //                 } else {
-        //                     $result = "error";
-        //                     $data = [];
-        //                 }
-        //             } else {
-        //                 http_response_code(400);
-        //                 $responseData = ["error" => "Dữ liệu không hợp lệ"];
-        //             }
-                
-        //             echo json_encode(['success' => $responseData, 'info' => $data]);
-        //             return;
-                
-        //         } else {
-        //             http_response_code(400);
-        //             echo json_encode(["error" => "Dữ liệu không hợp lệ"]);
-        //         }
-        //         $conn->close();
-        //     }
-        // }
-        
-        // function getCommentForUserFirst($id){
-        //     global $conn;
-        //     $selectQuery = "SELECT comment.*,
-        //                 users.realname,
-        //                 authority.authority_name,
-        //                 comment.createdAt AS createdAt
-        //             FROM comment
-        //             JOIN users ON comment.user_id = users.id
-        //             JOIN table_json ON table_json.id = comment.id_tablejson
-        //             JOIN authority ON comment.authority = authority.id
-        //             WHERE comment.id_tablejson  = $id and comment.authority = 1";
-        //     $result = mysqli_query($conn, $selectQuery);
-        //         $data = [];
-
-        //         while ($row = mysqli_fetch_assoc($result)) {
-        //             $data[] = $row;
-        //         }
-        //         http_response_code(200);
-        //         echo json_encode($data);
-        //     $conn->close();
-        // }
         function getCommentForUserFirst($id){
             global $conn;
             $selectQuery = "SELECT comment.*,
@@ -263,7 +142,6 @@
                     WHERE comment.aplication_id  = $id and comment.authority_id = 1";
             $result = mysqli_query($conn, $selectQuery);
                 $data = [];
-
                 while ($row = mysqli_fetch_assoc($result)) {
                     $data[] = $row;
                 }
@@ -303,47 +181,10 @@
                     http_response_code(500);
                     echo json_encode(["error" => "Thêm không thành công: " . mysqli_error($conn)]);
                 }
-
                 mysqli_stmt_close($stmt);
             }
         }
-        // function addComment($user_id,$id_tablejson,$table_id,$authority, $note, $createdAt)
-        // {
-        //     global $conn;
-        //     if ($_SERVER["REQUEST_METHOD"] === "POST") {
-        //         $commentPostData = json_decode(file_get_contents("php://input"));
-        //         $user_id = $commentPostData->user_id;
-        //         $id_tablejson = $commentPostData->id_tablejson;
-        //         $table_id = $commentPostData->table_id;
-        //         $authority = $commentPostData->authority;
-        //         $note = trim($commentPostData->note); // Loại bỏ các khoảng trắng dư thừa
-        //         if (empty($note)) {
-        //             http_response_code(400);
-        //             echo json_encode(["error" => "Không thể thêm comment: Nội dung trống"]);
-        //             exit();
-        //         }
-        //         $insertQuery = "INSERT INTO comment (user_id,id_tablejson,table_id,authority, note, createdAt) 
-        //                     VALUES (?, ?, ?, ?,?,NOW())";
-        //         $stmt = mysqli_prepare($conn, $insertQuery);
-
-        //         if (!$stmt) {
-        //             http_response_code(500);
-        //             echo json_encode(["error" => "Lỗi khi chuẩn bị câu lệnh: " . mysqli_error($conn)]);
-        //             exit();
-        //         }
-        //         mysqli_stmt_bind_param($stmt, "siiss", $user_id, $id_tablejson,$table_id,$authority, $note);
-
-        //         if (mysqli_stmt_execute($stmt)) {
-        //             http_response_code(201);
-        //             echo json_encode(["message" => "Thêm thành công"]);
-        //         } else {
-        //             http_response_code(500);
-        //             echo json_encode(["error" => "Thêm không thành công: " . mysqli_error($conn)]);
-        //         }
-
-        //         mysqli_stmt_close($stmt);
-        //     }
-        // }
+        
         function deleteCommentFirst($id){
             global $conn;
             $data = json_decode(file_get_contents("php://input"), true);
@@ -401,14 +242,12 @@
                 $insertQuery = "INSERT INTO comment (user_id,aplication_id,authority_id, note, createdAt) 
                             VALUES (?, ?, ?,?,NOW())";
                 $stmt = mysqli_prepare($conn, $insertQuery);
-
                 if (!$stmt) {
                     http_response_code(500);
                     echo json_encode(["error" => "Lỗi khi chuẩn bị câu lệnh: " . mysqli_error($conn)]);
                     exit();
                 }
                 mysqli_stmt_bind_param($stmt, "siis", $user_id, $aplication_id,$authority_id, $note);
-
                 if (mysqli_stmt_execute($stmt)) {
                     http_response_code(201);
                     echo json_encode(["message" => "Thêm thành công"]);
@@ -416,7 +255,6 @@
                     http_response_code(500);
                     echo json_encode(["error" => "Thêm không thành công: " . mysqli_error($conn)]);
                 }
-
                 mysqli_stmt_close($stmt);
             }
         }
@@ -452,7 +290,6 @@
                     WHERE comment.aplication_id  = $id and comment.authority_id = 3";
             $result = mysqli_query($conn, $selectQuery);
                 $data = [];
-
                 while ($row = mysqli_fetch_assoc($result)) {
                     $data[] = $row;
                 }
@@ -499,14 +336,12 @@
                 $insertQuery = "INSERT INTO comment (user_id,aplication_id,authority_id, note, createdAt) 
                             VALUES (?, ?, ?,?,NOW())";
                 $stmt = mysqli_prepare($conn, $insertQuery);
-
                 if (!$stmt) {
                     http_response_code(500);
                     echo json_encode(["error" => "Lỗi khi chuẩn bị câu lệnh: " . mysqli_error($conn)]);
                     exit();
                 }
                 mysqli_stmt_bind_param($stmt, "siis", $user_id, $aplication_id,$authority_id, $note);
-
                 if (mysqli_stmt_execute($stmt)) {
                     http_response_code(201);
                     echo json_encode(["message" => "Thêm thành công"]);
@@ -514,7 +349,6 @@
                     http_response_code(500);
                     echo json_encode(["error" => "Thêm không thành công: " . mysqli_error($conn)]);
                 }
-
                 mysqli_stmt_close($stmt);
             }
         }
