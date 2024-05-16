d8de9efe99b823bfb4d8110a37672e711c187749
Caily.Thuy <thuy_web@caily.com.vn>
2024-05-16 09:46:22 +0700

    up htm

--- client/src/renderer/pages/Application/register.tsx
+++ client/src/renderer/pages/Application/register.tsx
@@ -13,9 +13,12 @@ export const Register = ({ id }) => {
 
         // Phân tích JSON
         const parsedDataJson = JSON.parse(data.datajson);
-
-        if (parsedDataJson.formData && parsedDataJson.formData.length > 0) {
-          // Trường hợp 1: formData không rỗng
+        const hasFormData =
+          parsedDataJson.formData && parsedDataJson.formData.length > 0;
+        const hasTableData =
+          parsedDataJson.tableData && parsedDataJson.tableData.length > 0;
+        if (hasFormData && hasTableData) {
+          // Trường hợp: formData không rỗng và tableData không rỗng
           let periodValue = [];
           for (const item of parsedDataJson.formData) {
             if (item.label === '期間') {
@@ -24,11 +27,18 @@ export const Register = ({ id }) => {
           }
           parsedDataJson.periodValues = periodValue;
           setAccordionItems(parsedDataJson);
-        } else if (
-          parsedDataJson.tableData &&
-          parsedDataJson.tableData.length > 0
-        ) {
-          // Trường hợp 2: tableData không rỗng
+        } else if (hasFormData) {
+          // Trường hợp : formData không rỗng
+          let periodValue = [];
+          for (const item of parsedDataJson.formData) {
+            if (item.label === '期間') {
+              periodValue.push(item.value);
+            }
+          }
+          parsedDataJson.periodValues = periodValue;
+          setAccordionItems(parsedDataJson);
+        } else if (hasTableData) {
+          //trường hợp : tableData không rỗng
           setAccordionItems(parsedDataJson);
         } else {
           console.log('Cả formData và tableData đều rỗng');
@@ -45,108 +55,225 @@ export const Register = ({ id }) => {
   return (
     <>
       {accordionItems &&
-      accordionItems.formData &&
-      accordionItems.formData.length > 0 ? (
-        <div className="box-register">
-          <ul>
-            {accordionItems.formData.map((formDataItem, index) => {
-              if (formDataItem.label === '期間' && isPeriodDisplayed) {
-                return null;
-              }
-              if (formDataItem.label === '期間') {
-                isPeriodDisplayed = true;
-              }
-              if (!formDataItem.label) {
-                return null;
-              }
-              let valueToDisplay = formDataItem.value;
-              // Kiểm tra nếu label là "日間"
-              if (formDataItem.label === '日間') {
-                let duration = formDataItem.value.pop(); // Lấy giá trị "日間" ra khỏi mảng
-                valueToDisplay = formDataItem.value.join(' ~ '); // Nối giá trị còn lại
-                // valueToDisplay += `  ${duration.value}   ${duration.label}  `; // Thêm "日間" vào cuối
-              } else if (
-                Array.isArray(formDataItem.value) &&
-                formDataItem.value.length >= 2 // Kiểm tra nếu có ít nhất 2 phần tử
-              ) {
-                // Kiểm tra nếu cả hai phần tử đều là ngày
-                if (
-                  !isNaN(Date.parse(formDataItem.value[0])) &&
-                  !isNaN(Date.parse(formDataItem.value[1]))
-                ) {
-                  valueToDisplay = formDataItem.value.join(' ~ '); // Nếu cả hai đều là ngày, nối chúng với dấu "~"
-                } else {
-                  let [date, ...times] = formDataItem.value; // Tách ngày và thời gian
-                  let timeRange = times.join(' ~ '); // Nối các phần tử thời gian
-                  valueToDisplay = `${date} ${timeRange}`; // Hiển thị ngày và khoảng thời gian
-                }
-              } else if (Array.isArray(formDataItem.value)) {
-                valueToDisplay = formDataItem.value.join('  '); // Nối giá trị với dấu cách
-              }
+        (accordionItems.formData &&
+        accordionItems.formData.length > 0 &&
+        accordionItems.tableData &&
+        accordionItems.tableData.length > 0 ? (
+          <div>
+            <div className="box-register">
+              <ul>
+                {accordionItems.formData.map(
+                  (formDataItem: any, index: any) => {
+                    if (formDataItem.label === '期間' && isPeriodDisplayed) {
+                      return null;
+                    }
+                    if (formDataItem.label === '期間') {
+                      isPeriodDisplayed = true;
+                    }
+                    if (!formDataItem.label) {
+                      return null;
+                    }
+                    let valueToDisplay = formDataItem.value;
+                    // Kiểm tra nếu label là "日間"
+                    if (formDataItem.label === '日間') {
+                      let duration = formDataItem.value.pop(); // Lấy giá trị "日間" ra khỏi mảng
+                      valueToDisplay = formDataItem.value.join(' ~ '); // Nối giá trị còn lại
+                      // valueToDisplay += `  ${duration.value}   ${duration.label}  `; // Thêm "日間" vào cuối
+                    } else if (
+                      Array.isArray(formDataItem.value) &&
+                      formDataItem.value.length >= 2 // Kiểm tra nếu có ít nhất 2 phần tử
+                    ) {
+                      // Kiểm tra nếu cả hai phần tử đều là ngày
+                      if (
+                        !isNaN(Date.parse(formDataItem.value[0])) &&
+                        !isNaN(Date.parse(formDataItem.value[1]))
+                      ) {
+                        valueToDisplay = formDataItem.value.join(' ~ '); // Nếu cả hai đều là ngày, nối chúng với dấu "~"
+                      } else {
+                        let [date, ...times] = formDataItem.value; // Tách ngày và thời gian
+                        let timeRange = times.join(' ~ '); // Nối các phần tử thời gian
+                        valueToDisplay = `${date} ${timeRange}`; // Hiển thị ngày và khoảng thời gian
+                      }
+                    } else if (Array.isArray(formDataItem.value)) {
+                      valueToDisplay = formDataItem.value.join('  '); // Nối giá trị với dấu cách
+                    }
 
-              return (
-                <li key={index}>
-                  <div className="box-register__item">
-                    <span className="box-register__item__title">
-                      {formDataItem.label}
-                    </span>
-                    <span className="box-register__item__content">
-                      {valueToDisplay}
-                    </span>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      ) : (
-        <div className="box-register">
-          <div className="table tbl_custom table tbl_custom--register">
-            <div className="tbl_custom--03">
-              <table>
-                <thead>
-                  <tr>
-                    {accordionItems &&
-                      accordionItems.tableData &&
-                      Object.keys(accordionItems.tableData[0]).map(
-                        (key, index) => {
-                          // Bỏ qua cột 'id'
-                          if (key !== 'id') {
-                            return <th key={index}>{key}</th>;
-                          }
-                          return null;
-                        },
-                      )}
-                  </tr>
-                </thead>
-                <tbody>
-                  {accordionItems &&
-                    accordionItems.tableData &&
-                    accordionItems.tableData.map((tableDataItem, index) => (
-                      <tr key={index}>
-                        {Object.entries(tableDataItem).map(
-                          ([key, value], index) => {
-                            // Bỏ qua cột 'id'
-                            if (key !== 'id') {
-                              // Chuyển đổi 'date' thành 'yyyy-mm-dd' format
-                              if (key === 'date') {
-                                value = new Date(value)
-                                  .toISOString()
-                                  .split('T')[0];
+                    return (
+                      <li key={index}>
+                        <div className="box-register__item">
+                          <span className="box-register__item__title">
+                            {formDataItem.label}
+                          </span>
+                          <span className="box-register__item__content">
+                            {valueToDisplay}
+                          </span>
+                        </div>
+                      </li>
+                    );
+                  },
+                )}
+              </ul>
+            </div>
+            <div className="box-register">
+              <div className="table tbl_custom table tbl_custom--register">
+                <div className="tbl_custom--03">
+                  <table>
+                    <thead>
+                      <tr>
+                        {accordionItems &&
+                          accordionItems.tableData &&
+                          Object.keys(accordionItems.tableData[0]).map(
+                            (key, index) => {
+                              // Bỏ qua cột 'id'
+                              if (key !== 'id') {
+                                return <th key={index}>{key}</th>;
                               }
-                              return <td key={index}>{value}</td>;
+                              return null;
+                            },
+                          )}
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {accordionItems &&
+                        accordionItems.tableData &&
+                        accordionItems.tableData.map(
+                          (tableDataItem: any, index: any) => (
+                            <tr key={index}>
+                              {Object.entries(tableDataItem).map(
+                                ([key, value], index) => {
+                                  if (key !== 'id') {
+                                    if (key === 'date') {
+                                      // Kiểm tra xem giá trị ngày có hợp lệ không trước khi chuyển đổi
+                                      if (!isNaN(Date.parse(value))) {
+                                        value = new Date(value)
+                                          .toISOString()
+                                          .split('T')[0];
+                                      } else {
+                                        // Xử lý nếu giá trị không hợp lệ
+                                        value = 'Ngày không hợp lệ';
+                                      }
+                                    }
+                                    return <td key={index}>{value}</td>;
+                                  }
+                                  return null;
+                                },
+                              )}
+                            </tr>
+                          ),
+                        )}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : accordionItems.formData && accordionItems.formData.length > 0 ? (
+          //trường hợp : formData không rỗng
+
+          <div className="box-register">
+            <ul>
+              {accordionItems.formData.map((formDataItem: any, index: any) => {
+                if (formDataItem.label === '期間' && isPeriodDisplayed) {
+                  return null;
+                }
+                if (formDataItem.label === '期間') {
+                  isPeriodDisplayed = true;
+                }
+                if (!formDataItem.label) {
+                  return null;
+                }
+                let valueToDisplay = formDataItem.value;
+                // Kiểm tra nếu label là "日間"
+                if (formDataItem.label === '日間') {
+                  let duration = formDataItem.value.pop(); // Lấy giá trị "日間" ra khỏi mảng
+                  valueToDisplay = formDataItem.value.join(' ~ '); // Nối giá trị còn lại
+                  // valueToDisplay += `  ${duration.value}   ${duration.label}  `; // Thêm "日間" vào cuối
+                } else if (
+                  Array.isArray(formDataItem.value) &&
+                  formDataItem.value.length >= 2 // Kiểm tra nếu có ít nhất 2 phần tử
+                ) {
+                  // Kiểm tra nếu cả hai phần tử đều là ngày
+                  if (
+                    !isNaN(Date.parse(formDataItem.value[0])) &&
+                    !isNaN(Date.parse(formDataItem.value[1]))
+                  ) {
+                    valueToDisplay = formDataItem.value.join(' ~ '); // Nếu cả hai đều là ngày, nối chúng với dấu "~"
+                  } else {
+                    let [date, ...times] = formDataItem.value; // Tách ngày và thời gian
+                    let timeRange = times.join(' ~ '); // Nối các phần tử thời gian
+                    valueToDisplay = `${date} ${timeRange}`; // Hiển thị ngày và khoảng thời gian
+                  }
+                } else if (Array.isArray(formDataItem.value)) {
+                  valueToDisplay = formDataItem.value.join('  '); // Nối giá trị với dấu cách
+                }
+
+                return (
+                  <li key={index}>
+                    <div className="box-register__item">
+                      <span className="box-register__item__title">
+                        {formDataItem.label}
+                      </span>
+                      <span className="box-register__item__content">
+                        {valueToDisplay}
+                      </span>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        ) : (
+          <div className="box-register">
+            <div className="table tbl_custom table tbl_custom--register">
+              <div className="tbl_custom--03">
+                <table>
+                  <thead>
+                    <tr>
+                      {accordionItems &&
+                        accordionItems.tableData &&
+                        Object.keys(accordionItems.tableData[0]).map(
+                          (key, index) => {
+                            if (key !== 'id') {
+                              return <th key={index}>{key}</th>;
                             }
                             return null;
                           },
                         )}
-                      </tr>
-                    ))}
-                </tbody>
-              </table>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {accordionItems &&
+                      accordionItems.tableData &&
+                      accordionItems.tableData.map(
+                        (tableDataItem: any, index: any) => (
+                          <tr key={index}>
+                            {Object.entries(tableDataItem).map(
+                              ([key, value], index) => {
+                                if (key !== 'id') {
+                                  if (key === 'date') {
+                                    if (!isNaN(Date.parse(value))) {
+                                      value = new Date(value)
+                                        .toISOString()
+                                        .split('T')[0];
+                                    } else {
+                                      // Xử lý nếu giá trị không phải là ngày hợp lệ
+                                      value = 'Ngày không hợp lệ';
+                                    }
+                                  }
+                                  return <td key={index}>{value}</td>;
+                                }
+                                return null;
+                              },
+                            )}
+                          </tr>
+                        ),
+                      )}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        ))}
     </>
   );
 };
