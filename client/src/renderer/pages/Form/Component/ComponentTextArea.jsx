612460f751bf10c6176aced60b22a103b31d79f1
Caily.Tu <tu_caily@caily.com.vn>
2024-05-21 17:24:59 +0700

    Update validate

--- client/src/renderer/pages/Form/Component/ComponentTextArea.jsx
+++ client/src/renderer/pages/Form/Component/ComponentTextArea.jsx
@@ -1,4 +1,24 @@
-export default function ComponentTextArea(props){
+import { useImperativeHandle, forwardRef, useRef, useState } from "react";
+import { isValidText } from "../../../components/Validate";
+
+const ComponentTextArea = forwardRef((props, ref) => {
+  const [isValue, setIsValue] = useState();
+
+  const handleChange = (e) => {
+    setIsValue(e.target.value);
+  }
+
+  const textAreaRef = useRef(null);
+  useImperativeHandle(ref, () => ({
+    validate: () => {
+      let valid = true;
+      if (isValue.trim() === '' && props.required === true) {
+        valid = isValidText(isValue, props.label);
+      }
+      return valid;
+    },
+  }));
+
   return (
   <div className="c-form">
     <div className="c-form-inner">
@@ -7,9 +27,11 @@ export default function ComponentTextArea(props){
         {props.required === true ? <span className="c-form-label--required txt-red">（必須）</span> : ''}
       </label>
       <div className="c-form-content">
-        <textarea className="c-form-control" placeholder="入力してください" name={props.id} onChange={props.onHandle} aria-label={props.label} title={props.label} required={props.required}></textarea>
+        <textarea className="c-form-control" placeholder="入力してください" name={props.id} onChange={handleChange} value="" aria-label={props.label} title={props.label} required={props.required} ref={textAreaRef}/>
       </div>
     </div>
   </div>
   )
-}
+});
+
+export default ComponentTextArea;
