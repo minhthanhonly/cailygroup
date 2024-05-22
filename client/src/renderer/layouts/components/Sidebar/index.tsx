a3440c97dab2c5bfc01c2fd58f1ea05aeab420bc
Caily.QuocThinh <quocthinh_web@caily.com.vn>
2024-05-22 16:18:31 +0700

    update

--- client/src/renderer/layouts/components/Sidebar/index.tsx
+++ client/src/renderer/layouts/components/Sidebar/index.tsx
@@ -182,7 +182,7 @@ export const Sidebar = () => {
                 <span className="icn">
                   <FontAwesomeIcon icon={faCalendarDays} />
                 </span>
-                申請状況 <span className="boder_count">{countIdStatusOne}</span>
+                申請状況
               </NavLink>
             </li>
           )}
